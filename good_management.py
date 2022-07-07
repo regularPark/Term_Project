c67f8383c84f180648fc749d22db99af98108dc3
박정규 <srk99279@naver.com>
2022-07-07 15:13:36 +0900

    Update good_management.py

--- good_management.py
+++ good_management.py
@@ -1,3 +1,5 @@
+area = []
+
 def show_menu():
     print('*******************************************')
     print('1. Office Management')
@@ -16,6 +18,15 @@ def show_submenu():
     print('5. Back to main')
     print('*******************************************')
 
+def show_search_menu():
+    print('*******************************************')
+    print('1. Retrieve')
+    print('2. Search')
+    print('3. Update')
+    print('4. Delete')
+    print('5. Back to main')
+    print('*******************************************')    
+
 def office_manage():
     show_submenu()
 
@@ -30,6 +41,20 @@ def office_manage():
         else:
             pass
 
+def area_manage():
+    show_submenu()
+
+    n = int(input('Enter the number of the menu >'))
+    while True:
+        if n == 1:
+            pass
+        elif n == 2:
+            
+        elif n == 3:
+            pass
+        else:
+            pass
+
 def main_menu():
     while True:
         show_menu()
@@ -37,6 +62,13 @@ def main_menu():
         # 사무실 관리
         if n == 1:
             office_manage()
+        elif n == 2:
+            pass
+        elif n == 3:
+            pass
+        elif n == 4:
+            pass
+
 
 
 
